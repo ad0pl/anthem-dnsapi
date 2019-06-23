6eda5978e26454495e8d83b513cd7ff7ff8c1458
Eddie Bugg <zarhim@gmail.com>
2019-06-22 23:56:19 -0500

    More work on infoblox support

---
+++ flask-app/infoblox/errors.py
@@ -0,0 +1,12 @@
+class BadCredentials(Exception):
+    pass
+
+class BadRequstError(Exception):
+    pass
+
+class InvalidObjectType(Exception):
+    pass
+
+class ServerError(Exception):
+    pass
+
