a37dd4b10edbcd143dfc460ed32bdaa413edc073
Eddie Bugg <zarhim@gmail.com>
2019-06-23 02:54:15 -0500

    Got the url_for working

--- flask-app/api/apps/infoblox/endpoints/resources/record_host.py
+++ flask-app/api/apps/infoblox/endpoints/resources/record_host.py
@@ -1,11 +1,26 @@
 import logging
-from flask import g, current_app
+from flask import g, current_app, url_for
 from flask_restful import Resource, reqparse, abort
 from api.apps.extensions.rest import rest_error_response
 
 class record_host(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type = str, required = True,
+                help = 'No Hostname provided', location = 'json')
+        self.reqparse.add_argument('view', type = str, required = True,
+                help = 'No view provided', location = ['json', 'values'])
+        self.reqparse.add_argument('address', type = str, required = True,
+                help = 'No address provided', location = 'json')
+        self.reqparse.add_argument('change_control', type = str, required = True,
+                help = 'No change_control provided', location = 'json')
+        self.reqparse.add_argument('domain', type = str, location = 'json')
+
+        # Delete only really requires that we have change control
+        self.delparse = reqparse.RequestParser()
+        self.delparse.add_argument('change_control', type = str, required = True,
+                help = 'No change_control provided', location = 'json')
+
         super(record_host, self).__init__()
         self.logger = logging.getLogger(__name__)
 
@@ -32,7 +47,9 @@ class record_host(Resource):
             return rest_error_response(401)
         else:
             self.logger.debug("Auth cookie Found")
-        return { }, 200
+        return { 
+                "link": url_for('by_ref', view=view, domain=domain, name=name)
+                }, 200
 
     # Update
     def put(self, view=None, domain=None, name=None):
